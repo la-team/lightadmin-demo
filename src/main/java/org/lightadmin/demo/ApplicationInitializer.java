74ce84bce1010a7156526d46925d1324655e5ef1
max-dev <kharchenko.max@gmail.com>
2014-09-13 19:04:14 +0300

    Added Web Logging Configurer library from https://github.com/la-team/light-logging-configurer

--- src/main/java/org/lightadmin/demo/ApplicationInitializer.java
+++ src/main/java/org/lightadmin/demo/ApplicationInitializer.java
@@ -1,7 +1,6 @@
 package org.lightadmin.demo;
 
 import org.lightadmin.api.config.LightAdmin;
-import org.lightadmin.api.config.annotation.FileReference;
 import org.lightadmin.demo.configuration.ApplicationConfiguration;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.context.AbstractContextLoaderInitializer;
@@ -13,6 +12,8 @@ import javax.servlet.ServletException;
 
 import static java.util.EnumSet.of;
 import static javax.servlet.SessionTrackingMode.COOKIE;
+import static org.lightadmin.logging.configurer.LightConfigurerWebApplicationInitializer.LIGHT_CONFIGURER_BACK_TO_SITE_URL;
+import static org.lightadmin.logging.configurer.LightConfigurerWebApplicationInitializer.LIGHT_CONFIGURER_BASE_URL;
 import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
 
 @SuppressWarnings("unused")
@@ -21,6 +22,8 @@ public class ApplicationInitializer extends AbstractContextLoaderInitializer {
 
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
+        configureWebLogger(servletContext, "/logger");
+
         LightAdmin.configure(servletContext)
                 .baseUrl("/")
                 .backToSiteUrl("http://lightadmin.org")
@@ -39,5 +42,10 @@ public class ApplicationInitializer extends AbstractContextLoaderInitializer {
         webApplicationContext.register(ApplicationConfiguration.class);
         webApplicationContext.refresh();
         return webApplicationContext;
+    }
+
+    private static void configureWebLogger(ServletContext servletContext, String baseUrl) {
+        servletContext.setInitParameter(LIGHT_CONFIGURER_BASE_URL, baseUrl);
+        servletContext.setInitParameter(LIGHT_CONFIGURER_BACK_TO_SITE_URL, "http://lightadmin.org");
     }
 }
